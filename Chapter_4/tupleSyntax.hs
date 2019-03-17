8d17212798ecc5dd0cba75f51bcb78a0b3b4f8af
Helgard Richard Ferreira <hrfer3@gmail.com>
2019-03-17 15:01:35 +0200

    Experiment With Tuples

---
+++ Chapter_4/tupleSyntax.hs
@@ -0,0 +1,14 @@
+module TupleSyntax where
+
+fst' :: (a, b) -> a
+fst' (a, b) = a
+
+snd' :: (a, b) -> b
+snd' (a, b) = b
+
+tupFunc :: (Int, [a])
+    -> (Int, [a])
+    -> (Int, [a])
+
+tupFunc (a, b) (c, d) =
+    (a + c, b ++ d)
